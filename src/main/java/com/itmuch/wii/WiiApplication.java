2f5f0807605b05535c5559533396fad6f02c1a9d
youzhan <751642772@qq.com>
2021-09-28 22:01:15 +0800

    wii

---
+++ src/main/java/com/itmuch/wii/WiiApplication.java
@@ -0,0 +1,13 @@
+package com.itmuch.wii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiiApplication.class, args);
+    }
+
+}
